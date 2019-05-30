b0d8b73e5298a5a8b387dcd10487aec1962030c7
Branson Fox <32143148+bransonf@users.noreply.github.com>
2019-05-30 12:41:06 -0500

    refactor data

--- dashboard/server.R
+++ dashboard/server.R
@@ -1,16 +1,23 @@
 # Dashboard Server Logic
 
 library(shiny)
+library(shinyWidgets)
 library(leaflet)
-library(geojsonio)
+library(sf)
 
-# Load data and define palette
-stl <- geojsonio::geojson_read("stl_boundary.geojson", what = "sp")
-demog <- geojsonio::geojson_read("stl_demog.geojson", what = "sp")
-bins <- c(0, 20, 40, 60, 80, 100)
-pal <- colorBin("YlOrRd", domain = 0:100, bins = bins)
-inc_bins <- c(0, 15000, 25000, 45000, 60000, 100000)
-inc_pal <- colorBin("YlOrRd", domain = 0:75000, bins = inc_bins)
+# Load data and define palettes
+
+load("cardiff.rda")
+# using Jenks Natural Breaks
+inc_pal   <- colorBin("viridis", domain = 0:75000, bins = c(0,58786,32609,22880,45375,74425))
+pov_pal   <- colorBin("viridis", domain = 0:100, bins = c(0,14,24,35,46,61))
+hs_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,71,79,86,91,98))
+ba_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,15,29,47,61,77))
+unemp_pal <- colorBin("viridis", domain = 0:100, bins = c(0,6,11,18,26,35))
+home_pal  <- colorBin("viridis", domain = 0:100, bins = c(0,19,38,51,67,85))
+
+# define color palettes
+
 
 # Define server logic
 shinyServer(function(input, output) {
@@ -24,14 +31,16 @@ shinyServer(function(input, output) {
             setView(-90.2594, 38.6530, zoom = 11) -> leaf
             
       # add demographic layer
-      if(input$demog_select == "None"){clearShapes(leaf) %>% addPolygons(data = stl, fill = NA)}
-      else if(input$demog_select == "Median Income"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~inc_pal(med_income), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Poverty Rate"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pal(pov_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "High School Attainment"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pal(hs_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Bachelors Attainment"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pal(ba_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Unemployment Rate"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pal(unemploy_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Home Ownership"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pal(home_own_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      if(input$demog_select == "None"){clearShapes(leaf) %>% addPolygons(data = boundary, fill = NA)}
+      else if(input$demog_select == "Median Income"){         clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~inc_pal(med_income), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      else if(input$demog_select == "Poverty Rate"){          clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pov_pal(pov_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      else if(input$demog_select == "High School Attainment"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~hs_pal(hs_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      else if(input$demog_select == "Bachelors Attainment"){  clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~ba_pal(ba_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      else if(input$demog_select == "Unemployment Rate"){     clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~unemp_pal(unemploy_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      else if(input$demog_select == "Home Ownership"){        clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~home_pal(home_own_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
             
+      # add Environment Variables
+      #if("ATMs" %in% input$env_chk){leaf %>% addCircleMarkers(data = atm)}
   
     })
 
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -1,7 +1,12 @@
 # Dashboard User Interface
 
 library(shiny)
+library(shinyWidgets)
 library(leaflet)
+library(sf)
+
+# Get Current Month
+cur_month <- month.name[as.numeric(format(Sys.Date(), "%m"))]
 
 # Set Text Here
 cardiff <- HTML("<h3 class=heading>The Cardiff Model</h3>", "<p class=sans> The U.S. Department of Justice found that more than half of violent crime in the United States goes unreported to law enforcement. This is a clear limitation to understanding where violence occurs. The Cardiff Model combines police and hospital data on violence. As a result, key stakeholders from policy makers to public health experts can more accurately assess violence in the community. <a href='https://www.cdc.gov/violenceprevention/publichealthissue/fundedprograms/cardiffmodel/whatis.html'>Read More at the CDC</a></p>")
@@ -24,15 +29,31 @@ shinyUI(fluidPage(
         column(3, HTML("<h5 class=heading>Select Data to Map:</h5>"),
                checkboxGroupInput("crime_chk", "Crime",
                                     choices = c("Homicide", "Rape", "Robbery", "Assault")),
-               checkboxGroupInput("inj_chk", "Violent Injury",
-                                    choices = c("Gun Shot", "Stabbing", "Rape")),
+               pickerInput("inj_chk", "Violent Injury",
+                                    choices = c("Gun Shot", "Stabbing", "Rape"),
+                                    options = list(
+                                      `actions-box` = TRUE, 
+                                      size = 10,
+                                      `selected-text-format` = "count > 3"
+                                    ), 
+                                    multiple = TRUE
+                           ),
+               pickerInput("env_chk", "Environment",
+                           choices = c("ATMs", "Alcohol", "Transit", "Gas Stations", "Schools", "Vacancy", "Venues"),
+                           options = list(
+                             `actions-box` = TRUE, 
+                             size = 10,
+                             `selected-text-format` = "count > 3"
+                           ), 
+                           multiple = TRUE
+               ),
                selectInput("demog_select", "Demographic",
                                     choices = c("Median Income", "Poverty Rate", "High School Attainment", "Bachelors Attainment", "Unemployment Rate", "Home Ownership", "None"),
                             selected = "None"),
                checkboxInput("legend", "Show Legend"),
-               dateRangeInput("date_rng", "Select a Date Range:"),
+               sliderTextInput("month", "Select a Month:", month.name, cur_month),
                sliderInput("age_slide", "Select an Age Range:", 13, 100, value = c(13, 100), step = 1),
-               submitButton("Submit")
+               submitButton("Update")
         )
     )
 ))
