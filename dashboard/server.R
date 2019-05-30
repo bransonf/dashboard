be2fa34fcb45dcbea89168afd8b2171a28d81165
Branson Fox <32143148+bransonf@users.noreply.github.com>
2019-05-30 16:05:57 -0500

    navbar ui, style and core data

--- dashboard/server.R
+++ dashboard/server.R
@@ -3,6 +3,7 @@
 library(shiny)
 library(shinyWidgets)
 library(leaflet)
+library(leaflet.extras)
 library(sf)
 
 # Load data and define palettes
@@ -10,38 +11,56 @@ library(sf)
 load("cardiff.rda")
 # using Jenks Natural Breaks
 inc_pal   <- colorBin("viridis", domain = 0:75000, bins = c(0,58786,32609,22880,45375,74425))
-pov_pal   <- colorBin("viridis", domain = 0:100, bins = c(0,14,24,35,46,61))
-hs_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,71,79,86,91,98))
-ba_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,15,29,47,61,77))
-unemp_pal <- colorBin("viridis", domain = 0:100, bins = c(0,6,11,18,26,35))
-home_pal  <- colorBin("viridis", domain = 0:100, bins = c(0,19,38,51,67,85))
-
-# define color palettes
-
+pov_pal   <- colorBin("viridis", domain = 0:100, bins = c(0,14,24,35,46,62))
+hs_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,71,79,86,91,99))
+ba_pal    <- colorBin("viridis", domain = 0:100, bins = c(0,15,29,47,61,78))
+unemp_pal <- colorBin("viridis", domain = 0:100, bins = c(0,6,11,18,26,36))
+home_pal  <- colorBin("viridis", domain = 0:100, bins = c(0,19,38,51,67,86))
 
 # Define server logic
 shinyServer(function(input, output) {
-
+  
     # draw a map
     output$map <- renderLeaflet({
+        # basemap and attribution case
+        bm <- switch(input$base, "Terrain" = "http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg", "No Labels" =  "http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png")
+        at <- switch(input$base, "Terrain" = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.', "No Labels" =  '<a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, © <a href="https://carto.com/attribution">CARTO</a>')
+      
         leaflet() %>%
-            addProviderTiles(providers$Stamen.Terrain,
-                             options = providerTileOptions(noWrap = TRUE)
-            ) %>%
+            enableTileCaching() %>%
+            addTiles(bm, attribution = at) %>%
             setView(-90.2594, 38.6530, zoom = 11) -> leaf
             
       # add demographic layer
-      if(input$demog_select == "None"){clearShapes(leaf) %>% addPolygons(data = boundary, fill = NA)}
-      else if(input$demog_select == "Median Income"){         clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~inc_pal(med_income), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Poverty Rate"){          clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~pov_pal(pov_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "High School Attainment"){clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~hs_pal(hs_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Bachelors Attainment"){  clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~ba_pal(ba_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Unemployment Rate"){     clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~unemp_pal(unemploy_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
-      else if(input$demog_select == "Home Ownership"){        clearShapes(leaf) %>% addPolygons(data = demog, fillColor = ~home_pal(home_own_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.7)}
+      if(input$demog_select == "None"){                       leaf %>% addPolygons(data = boundary, fill = NA) -> leaf}
+      else if(input$demog_select == "Median Income"){         leaf %>% addPolygons(data = demog, fillColor = ~inc_pal(med_income), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
+      else if(input$demog_select == "Poverty Rate"){          leaf %>% addPolygons(data = demog, fillColor = ~pov_pal(pov_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
+      else if(input$demog_select == "High School Attainment"){leaf %>% addPolygons(data = demog, fillColor = ~hs_pal(hs_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
+      else if(input$demog_select == "Bachelors Attainment"){  leaf %>% addPolygons(data = demog, fillColor = ~ba_pal(ba_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
+      else if(input$demog_select == "Unemployment Rate"){     leaf %>% addPolygons(data = demog, fillColor = ~unemp_pal(unemploy_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
+      else if(input$demog_select == "Home Ownership"){        leaf %>% addPolygons(data = demog, fillColor = ~home_pal(home_own_pct), weight = 2, opacity = 1, color = "white", dashArray = "3", fillOpacity = 0.5) -> leaf}
             
-      # add Environment Variables
-      #if("ATMs" %in% input$env_chk){leaf %>% addCircleMarkers(data = atm)}
+      # add environment variables
+      if("Venues" %in% input$env_chk){        leaf %>% addPolygons(data = venues) -> leaf}
+      if("Parks" %in% input$env_chk){         leaf %>% addPolygons(data = park) -> leaf}
+      
+      if("ATMs" %in% input$env_chk){          leaf %>% addCircleMarkers(data = atm) -> leaf}
+      if("Bars" %in% input$env_chk){          leaf %>% addCircleMarkers(data = bar) -> leaf}
+      if("Clubs" %in% input$env_chk){         leaf %>% addCircleMarkers(data = club) -> leaf}
+      if("Liquor Stores" %in% input$env_chk){ leaf %>% addCircleMarkers(data = liquor) -> leaf}
+      if("Gas Stations" %in% input$env_chk){  leaf %>% addCircleMarkers(data = gas) -> leaf}
+      if("Grocery Stores" %in% input$env_chk){leaf %>% addCircleMarkers(data = food) -> leaf}
+      if("Bus Stops" %in% input$env_chk){     leaf %>% addCircleMarkers(data = bus) -> leaf}
+      if("Schools" %in% input$env_chk){       leaf %>% addCircleMarkers(data = school) -> leaf}
+      #TODO get data if("Vacancy" %in% input$env_chk){       leaf %>% addCircleMarkers(data = vacancy) -> leaf}
+      
+      #TODO Crime and Injury Data
+      # add crime Data
+      
+      # add injury data
   
+      # print map
+      return(leaf)
     })
 
 
--- dashboard/ui.R
+++ dashboard/ui.R
@@ -7,53 +7,63 @@ library(sf)
 
 # Get Current Month
 cur_month <- month.name[as.numeric(format(Sys.Date(), "%m"))]
+# Set Cardiff Text Here
+cardiff <- HTML("<p class=sans> The U.S. Department of Justice found that more than half of violent crime in the United States goes unreported to law enforcement. This is a clear limitation to understanding where violence occurs. The Cardiff Model combines police and hospital data on violence. As a result, key stakeholders from policy makers to public health experts can more accurately assess violence in the community. <a href='https://www.cdc.gov/violenceprevention/publichealthissue/fundedprograms/cardiffmodel/whatis.html'>Read More at the CDC</a></p>")
 
-# Set Text Here
-cardiff <- HTML("<h3 class=heading>The Cardiff Model</h3>", "<p class=sans> The U.S. Department of Justice found that more than half of violent crime in the United States goes unreported to law enforcement. This is a clear limitation to understanding where violence occurs. The Cardiff Model combines police and hospital data on violence. As a result, key stakeholders from policy makers to public health experts can more accurately assess violence in the community. <a href='https://www.cdc.gov/violenceprevention/publichealthissue/fundedprograms/cardiffmodel/whatis.html'>Read More at the CDC</a></p>")
+# Begin Navbar page
+shinyUI(
+  navbarPage("Cardiff STL", fluid = TRUE,
+           
+           tabPanel("Map",
+                    headerPanel(HTML("<h1 class=title>Cardiff Map</h1>")),
+                    # Add a Row
+                    fluidRow(
+                      # Add Columns Within Row
+                      column(9, leafletOutput("map", height = "600px")),
+                      column(3, HTML("<h5 class=heading>Select Data to Map:</h5>"),
+                             selectInput("base", "Basemap", c("Terrain", "No Labels"), selected = "Terrain"),
+                             checkboxGroupInput("crime_chk", "Crime",
+                                                choices = c("Homicide", "Rape", "Robbery", "Assault")),
+                             pickerInput("inj_chk", "Violent Injury",
+                                         choices = c("Gun Shot", "Stabbing", "Rape"),
+                                         options = list(
+                                           `actions-box` = TRUE, 
+                                           size = 10,
+                                           `selected-text-format` = "count > 3"
+                                         ), 
+                                         multiple = TRUE
+                             ),
+                             pickerInput("env_chk", "Environment",
+                                         choices = c("ATMs", "Bars", "Clubs", "Liquor Stores", "Gas Stations", "Grocery Stores", "Bus Stops", "Schools", "Vacancy", "Venues", "Parks"),
+                                         options = list(
+                                           `actions-box` = TRUE, 
+                                           size = 10,
+                                           `selected-text-format` = "count > 3"
+                                         ), 
+                                         multiple = TRUE
+                             ),
+                             selectInput("demog_select", "Demographic",
+                                         choices = c("Median Income", "Poverty Rate", "High School Attainment", "Bachelors Attainment", "Unemployment Rate", "Home Ownership", "None"),
+                                         selected = "None"),
+                             checkboxInput("legend", "Show Legend"),
+                             sliderTextInput("month", "Select a Month:", month.name, cur_month),
+                             sliderInput("age_slide", "Select an Age Range:", 13, 100, value = c(13, 100), step = 1),
+                             submitButton("Update")
+                      )
+                    )
+           ),
+           tabPanel("About",
+                    # This links to the CSS stylesheet
+                    tags$head(
+                      tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+                      tags$title("Cardiff Dashboard") # Page Title. Need to Add Favicon Still
+                      
+                    ),
+                    headerPanel(HTML("<h1 class=title>The Cardiff Model</h1>")),
+                    cardiff
+           )
+  )
+)
 
-shinyUI(fluidPage(
+   
     
-    # This links to the CSS stylesheet
-    tags$head(
-        tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
-        tags$title("Cardiff Dashboard") # Page Title. Need to Add Favicon Still
-        
-    ),
-    
-    # Application title
-    headerPanel(HTML("<h1 class=title>Cardiff Dashboard</h1>")),
-    # Add a Row
-    fluidRow(
-        # Add Columns Within Row
-        column(9, leafletOutput("map", height = "500px"), cardiff),
-        column(3, HTML("<h5 class=heading>Select Data to Map:</h5>"),
-               checkboxGroupInput("crime_chk", "Crime",
-                                    choices = c("Homicide", "Rape", "Robbery", "Assault")),
-               pickerInput("inj_chk", "Violent Injury",
-                                    choices = c("Gun Shot", "Stabbing", "Rape"),
-                                    options = list(
-                                      `actions-box` = TRUE, 
-                                      size = 10,
-                                      `selected-text-format` = "count > 3"
-                                    ), 
-                                    multiple = TRUE
-                           ),
-               pickerInput("env_chk", "Environment",
-                           choices = c("ATMs", "Alcohol", "Transit", "Gas Stations", "Schools", "Vacancy", "Venues"),
-                           options = list(
-                             `actions-box` = TRUE, 
-                             size = 10,
-                             `selected-text-format` = "count > 3"
-                           ), 
-                           multiple = TRUE
-               ),
-               selectInput("demog_select", "Demographic",
-                                    choices = c("Median Income", "Poverty Rate", "High School Attainment", "Bachelors Attainment", "Unemployment Rate", "Home Ownership", "None"),
-                            selected = "None"),
-               checkboxInput("legend", "Show Legend"),
-               sliderTextInput("month", "Select a Month:", month.name, cur_month),
-               sliderInput("age_slide", "Select an Age Range:", 13, 100, value = c(13, 100), step = 1),
-               submitButton("Update")
-        )
-    )
-))
