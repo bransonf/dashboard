d104a9a2f825de5665df0a66029f2dd43e10c53c
Branson Fox <32143148+bransonf@users.noreply.github.com>
2019-06-21 10:36:53 -0500

    move report UI to seperate file

---
+++ dashboard/report_UI.R
@@ -0,0 +1,38 @@
+# report_UI
+
+reportUI <- function(rep_info){
+  column(12, align = 'center',
+         HTML("<h4 class=sans>Select Options Below</h4>"),
+         fluidRow(align = "center",
+                  pickerInput("rep_table", "Table(s)",
+                              choices = c("Summary Table","District Table", "Neighborhood Table"),
+                              options = list(
+                                `actions-box` = TRUE, 
+                                size = 10,
+                                `selected-text-format` = "count > 3"
+                              ),multiple = TRUE
+                  ),
+                  pickerInput("rep_maps", "Map(s)",
+                              choices = c("District Map", "Neighborhood Map", "Point Map", "Heat Map"),
+                              options = list(
+                                `actions-box` = TRUE, 
+                                size = 10,
+                                `selected-text-format` = "count > 3"
+                              ),multiple = TRUE
+                  ),
+                  pickerInput("rep_crime", "Crime(s)",
+                              choices = c("Homicide","Rape", "Assault", "Robbery", "Non-Violent Crimes"),
+                              options = list(
+                                `actions-box` = TRUE, 
+                                size = 10,
+                                `selected-text-format` = "count > 3"
+                              ),multiple = TRUE
+                  ),
+                  radioButtons("rep_year", "Select a Year:", c(2018, 2019), 2019, inline = TRUE),
+                  uiOutput("rep_month"),
+                  HTML("<h4 class=sans>Generate Report</h4>"),
+                  downloadButton('report', "Download")
+         ),
+         fluidRow(rep_info)
+  )
+}
